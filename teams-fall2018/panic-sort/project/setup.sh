587d94e1d6910db6516ff2130aa9a38f1e44f366
Kevin Liu <kevinrxliu@gmail.com>
2018-09-26 05:23:48 -0700

    made a script for installing all our dependencies

---
+++ teams-fall2018/panic-sort/project/setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install pdftk
+sudo apt-get install poppler-utils
+sudo apt-get install ghostscript
+sudo apt-get install tesseract-ocr
+npm install pdf-extract
+npm install express
+npm i -S formidable
+npm i eyes
